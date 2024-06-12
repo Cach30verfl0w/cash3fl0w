9772f50f707aeada48035b2caf76be93a12c6d00
cach30verfl0w <cach30verfl0w@gmail.com>
2024-06-12 16:48:01 +0200

    Add documentation to secure heap

--- kmp-advcrypto/src/linuxX64Main/kotlin/io/karma/advcrypto/linux/utils/SecureHeap.kt
+++ kmp-advcrypto/src/linuxX64Main/kotlin/io/karma/advcrypto/linux/utils/SecureHeap.kt
@@ -26,31 +26,69 @@ import libssl.CRYPTO_secure_malloc_init
 import libssl.CRYPTO_secure_malloc_initialized
 import libssl.ERR_func_error_string
 import libssl.ERR_get_error
-import kotlin.experimental.ExperimentalNativeApi
 
 /**
  * This class is a high-level wrapper around the secure heap provided by OpenSSL with the
  * `openssl/crypto.h` header. This heap is used to store sensitive information like keys
  * or other secrets.
  *
+ * Secure heap memory has a few protection against memory leakage. First, this memory can not be
+ * swapped to the disk and is not being written into core dumps. Also, the data gets deleted while
+ * freeing.
+ *
  * @author Cedric Hammes
  * @since  12/06/2024
  */
 @OptIn(ExperimentalForeignApi::class, ExperimentalStdlibApi::class)
-class OpenSSLSecureHeap(size: ULong, minSize: ULong): AutoCloseable {
+class SecureHeap(size: ULong, minSize: ULong): AutoCloseable {
+
+    /**
+     * This constructor initializes the secure heap if no secure heap was already initialized. If
+     * it's already initialized, we simply skip this step.
+     *
+     * @author Cedric Hammes
+     * @since  12/06/2024
+     */
     init {
         if (CRYPTO_secure_malloc_initialized() != 1) {
             CRYPTO_secure_malloc_init(size, minSize)
         }
     }
 
+    /**
+     * This method allocates a memory section with the specified size in the secure heap. If a error
+     * occurs, this function throws an exception.
+     *
+     * @param size The size of the memory section in bytes
+     * @return     A pointer to the allocated memory
+     *
+     * @author Cedric Hammes
+     * @since  12/06/2024
+     */
     fun allocate(size: ULong): COpaquePointer = CRYPTO_secure_malloc(size, this.toString(), 47)
         ?: throw Exception(ERR_func_error_string(ERR_get_error())?.toKString())
 
+    /**
+     * This method frees the allocated memory from the secure heap. This also deletes the data
+     * stored in the memory section.
+     *
+     * @param size    The size of the memory allocated
+     * @param pointer The pointer to the memory itself
+     *
+     * @author Cedric Hammes
+     * @since  12/06/2024
+     */
     fun free(size: ULong, pointer: COpaquePointer) {
         CRYPTO_secure_clear_free(pointer, size, this.toString(), 51)
     }
 
+    /**
+     * This method un-initializes the secure allocation if the security allocation was initialized
+     * before/is not uninitialized.
+     *
+     * @author Cedric Hammes
+     * @since  12/06/2024
+     */
     override fun close() {
         if (CRYPTO_secure_malloc_initialized() == 1) {
             CRYPTO_secure_malloc_done()
--- kmp-advcrypto/src/linuxX64Test/kotlin/io.karma.advcrypto.linux.tests/SecureHeapTests.kt
+++ kmp-advcrypto/src/linuxX64Test/kotlin/io.karma.advcrypto.linux.tests/SecureHeapTests.kt
@@ -1,6 +1,6 @@
 package io.karma.advcrypto.linux.tests
 
-import io.karma.advcrypto.linux.utils.OpenSSLSecureHeap
+import io.karma.advcrypto.linux.utils.SecureHeap
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.ULongVar
 import kotlinx.cinterop.pointed
@@ -15,7 +15,7 @@ class SecureHeapTests {
 
     @Test
     fun test() {
-        OpenSSLSecureHeap(ULong.SIZE_BYTES.toULong(), 0U).use { heap ->
+        SecureHeap(ULong.SIZE_BYTES.toULong(), 0U).use { heap ->
             val pointer = heap.allocate(8U).reinterpret<ULongVar>()
             pointer.pointed.value = 100U
             assert(pointer.pointed.value == 100UL)
