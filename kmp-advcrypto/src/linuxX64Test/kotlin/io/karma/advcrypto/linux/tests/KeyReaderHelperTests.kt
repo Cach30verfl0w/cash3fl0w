1fc018909baab824196f3a5cc997cdd1b9ee2341
cach30verfl0w <cach30verfl0w@gmail.com>
2024-06-14 20:03:36 +0200

    Fix error in tests

--- kmp-advcrypto/src/linuxX64Test/kotlin/io/karma/advcrypto/linux/tests/KeyReaderHelperTests.kt
+++ kmp-advcrypto/src/linuxX64Test/kotlin/io/karma/advcrypto/linux/tests/KeyReaderHelperTests.kt
@@ -10,11 +10,10 @@ import okio.Path.Companion.toPath
 import kotlin.experimental.ExperimentalNativeApi
 import kotlin.test.Test
 
-@OptIn(ExperimentalNativeApi::class)
+@OptIn(ExperimentalNativeApi::class, InsecureCryptoApi::class)
 class KeyReaderHelperTests {
     private val fileSystem = FileSystem.SYSTEM
-
-    @OptIn(InsecureCryptoApi::class)
+    
     @Test
     fun testPEM() {
         fileSystem.read("./testkeys/rsa-private-key.pem".toPath()) {
